a2b2f39616aa6390036edd34ed0ef898eb66b358
AlexeiLuchian <alexeiluchianx@gmail.com>
2025-08-14 01:25:13 +0300

    Create project and finish computer choice function

---
+++ javascript.js
@@ -0,0 +1,20 @@
+function getComputerChoice() {
+    let randomNumber = Math.floor(Math.random() * 3);
+    let computerChoice = undefined;
+    switch (randomNumber){
+        case 0:
+            computerChoice = "rock";
+            break;
+        case 1:
+            computerChoice = "paper";
+            break;
+        case 2:
+            computerChoice = "scissors";
+            break;
+    }
+    return computerChoice
+}
+
+
+
+console.log(getComputerChoice());
